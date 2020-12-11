b478fca7094cdc4754641281076241c8607aba05
Avery Follett <avery.follett@mymail.champlain.edu>
2020-12-11 16:29:26 -0500

    implemented message sending functionality

--- app/src/main/java/com/example/project3/MainActivity.kt
+++ app/src/main/java/com/example/project3/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.project3
 
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.json.JSONObject
-import java.lang.Exception
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var httpClient: OkHttpClient
     private lateinit var chitChatAdapter: ChitChatAdapter
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var newMessageEditText: EditText
+    private lateinit var sendButton: Button
 
     private lateinit var chats: ArrayList<Chat>
 
-    private val chitChatURL = "https://www.stepoutnyc.com/chitchat?method=request.get&key=fa070b3c-0685-431d-9fe2-e156bcbcfadb&client=avery.follett@mymail.champlain.edu"
+    private val chitChatURL = "https://www.stepoutnyc.com/chitchat"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +39,18 @@ class MainActivity : AppCompatActivity() {
         httpClient = OkHttpClient()
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
         swipeRefreshLayout.setOnRefreshListener { refresh() }
+
+        newMessageEditText = findViewById(R.id.newMessageEditText)
+        sendButton = findViewById(R.id.sendButton)
+
+        sendButton.setOnClickListener {
+            if (newMessageEditText.text.toString() != "") {
+                post(newMessageEditText.text.toString())
+                newMessageEditText.text.clear()
+                refresh()
+            }
+        }
+
         refresh()
     }
 
@@ -43,10 +59,22 @@ class MainActivity : AppCompatActivity() {
         rt.execute()
     }
 
+    private fun post(message: String) {
+        val pt = PostMessageTask(message)
+        pt.execute()
+    }
+
     private inner class RefreshTask : AsyncTask<Void?, Void?, String>() {
         override fun doInBackground(vararg voids: Void?): String {
+            var urlBuilder = chitChatURL.toHttpUrlOrNull()?.newBuilder()
+            if (urlBuilder != null) {
+                urlBuilder.addQueryParameter("key", "fa070b3c-0685-431d-9fe2-e156bcbcfadb")
+                urlBuilder.addQueryParameter("client", "avery.follett@mymail.champlain.edu")
+            }
+            var getURL = urlBuilder?.build().toString()
+
             val request: Request = Request.Builder()
-                .url(chitChatURL)
+                .url(getURL)
                 .build()
             try {
                 httpClient.newCall(request).execute().use { response -> return response.body!!.string() }
@@ -64,7 +92,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private inner class PostMessageTask(private var message: String) : AsyncTask<Void?, Void?, String>() {
+        override fun doInBackground(vararg voids: Void?): String {
+
+            val requestBody: RequestBody = MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("key", "fa070b3c-0685-431d-9fe2-e156bcbcfadb")
+                    .addFormDataPart("client", "avery.follett@mymail.champlain.edu")
+                    .addFormDataPart("message", message)
+                    .build()
+
+            val request: Request = Request.Builder()
+                    .url(chitChatURL)
+                    .post(requestBody)
+                    .build()
+            return try {
+                httpClient.newCall(request).execute()
+                "Posted"
+            } catch (e: Exception) {
+                e.printStackTrace()
+                "Error: Could not complete request."
+            }
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+        }
+    }
+
     private fun parseJSON(jsonString: String) {
+        chats.clear()
         try {
             val responseObject = JSONObject(jsonString)
             val items = responseObject.getJSONArray("messages")
